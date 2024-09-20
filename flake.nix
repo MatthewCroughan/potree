6fab51523ab5d5e463fc4d0753346c1eb6763b59
matthewcroughan <matt@croughan.sh>
2024-09-20 18:01:45 +0100

    use aarch64-linux instead

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
   outputs = { self, dream2nix }:
-  rec { packages.x86_64-linux.default = self.packages.x86_64-linux.potree; overlays.default = (self: super: { potree = packages.x86_64-linux.default; }); } //
+  rec { packages.aarch64-linux.default = self.packages.aarch64-linux.potree; overlays.default = (self: super: { potree = packages.aarch64-linux.default; }); } //
     dream2nix.lib.makeFlakeOutputs {
-      systems = ["x86_64-linux"];
+      systems = ["aarch64-linux"];
       config.projectRoot = ./.;
       source = ./.;
       projects = ./projects.toml;
